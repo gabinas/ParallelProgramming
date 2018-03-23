83fa38964c0967ecc35bab56ba7755ca14470524
Gabriela Lugo <gabrielalugobaez@hotmail.com>
2018-03-23 16:17:30 -0700

    Final Version Task1 Tiskin McColl

--- Week4/Task1_TiskinMcColl.c
+++ Week4/Task1_TiskinMcColl.c
@@ -3,69 +3,6 @@
 #include "include/mcbsp.h"
 unsigned int P;
 
-/*
-This program is supposed to complete the Two-Phase broadcast. 
-However, it only goes through the first phase and doesn't complete the second one.
-Later on we fixed this by synchronizing the processors after the last loop.
-
-*/
-
-typedef struct node{
-	int data;
-	struct node *next;
-}node_t;
-
-typedef void (*callback)(node_t* data);
-
-node_t* create(int data,node_t* next){
-    node_t* new_node = malloc(sizeof(node_t));
-    if(new_node == NULL)
-    {
-        printf("Error creating a new node.\n");
-        exit(0);
-    }
-    new_node->data = data;
-    new_node->next = next;
- 
-    return new_node;
-}
-
-void print_list(node_t * head){
-	node_t * current = head;
-	while(current!=NULL){
-		printf("%d\n",current->data);
-		current = current->next;
-	}
-}
-
-void push(node_t* head, int data){
-    /* go to the last node */
-    node_t *cursor = head;
-    while(cursor->next != NULL)
-        cursor = cursor->next;
- 
-    /* create a new node */
-    cursor->next = malloc(sizeof(node_t));
-   	cursor->next->data = data; 
-    cursor->next->next = NULL;
-}
-
-void display(node_t* n){
-    if(n != NULL)
-        printf("%d ", n->data);
-}
-
-int count(node_t* head){
-    node_t *cursor = head;
-    int c = 0;
-    while(cursor != NULL)
-    {
-        c++;
-        cursor = cursor->next;
-    }
-    return c;
-}
-
 int * createArray(int pID, int size) {
 	int *dataAddress = calloc(size, sizeof(int));
 	if(pID == 0){
@@ -124,6 +61,35 @@ int cmpfunc(const void * a, const void * b){
 	return ( *(int*)a - *(int*)b );
 }
 
+int * merge(int a1[], int a2[], int size1, int size2){
+	int c1 = 0,c2 = 0, m = 0;
+	int *merged = calloc(size1+size2,sizeof(int));
+	while(c1 < size1 && c2 < size2){
+		if(a1[c1] > a2[c2]){
+			merged[m] = a2[c2];
+			m++;
+			c2++;
+		} else {
+			merged[m] = a1[c1];
+			m++;
+			c1++;
+		}
+	}
+
+	while(c1 < size1){
+		merged[m] = a1[c1];
+		m++;
+		c1++;
+	}
+	while(c2 < size2){
+		merged[m] = a2[c2];
+		m++;
+		c2++;
+	}
+
+	return merged;
+}
+
 void  sort(){
 	bsp_begin(P);
 	
@@ -147,35 +113,23 @@ void  sort(){
 	localSample[P-1] = arr[size-2];
 	localSample[P] = arr[size-1];
 	bsp_push_reg(localSample, (P+1)*sizeof(int));
-
-	//Sharing local sample with all processor
-	node_t * p0 = calloc(P+1, sizeof(int));
-	node_t * p1 = calloc(P+1, sizeof(int));
-	node_t * p2 = calloc(P+1, sizeof(int));
-	node_t * p3 = calloc(P+1, sizeof(int));
 	bsp_sync();
 
+	//Sharing local sample with all processor
+	int * p0 = calloc(P+1,sizeof(int));
+	int * p1 = calloc(P+1,sizeof(int));
+	int * p2 = calloc(P+1,sizeof(int));
+	int * p3 = calloc(P+1,sizeof(int));
 
+	//Receives all local samples as arrays
 	bsp_get(0,localSample,0,p0,(P+1)*sizeof(int));
 	bsp_get(1,localSample,0,p1,(P+1)*sizeof(int));
 	bsp_get(2,localSample,0,p2,(P+1)*sizeof(int));
 	bsp_get(3,localSample,0,p3,(P+1)*sizeof(int));
 	bsp_sync();
 
-
-
-	//push(p0,2);
-
-	/*
-	for(int i = 0; i < P; i++){
-		bsp_put(i,localSample,LS,p*(P+1)*sizeof(int),(P+1)*sizeof(int));
-	}
-	bsp_sync();
-	*/
 	//Local Sample Merge
-
-	/*
-	//qsort(LS, P*(P+1), sizeof(int), cmpfunc);
+	LS = merge( merge(p0,p1,P+1,P+1) , merge(p2,p3,P+1,P+1) , (P+1)*2 , (P+1)*2 );
 
 	//Pick Global Separators
 	int *global = calloc(P+1, sizeof(int));
@@ -186,41 +140,50 @@ void  sort(){
 	global[4] = LS[(int)(P*(P+1)-1)];
 
 	//Partition Local blocks according to global separators
-	int *partOne = calloc(40,sizeof(int));
-	int *partTwo = calloc(40,sizeof(int));
-	int *partThree = calloc(40,sizeof(int));
-	int *partFour = calloc(40,sizeof(int));
+	int Route[P][size];	//Will store all partitions of the array into processors
+	int *Length = calloc(P,sizeof(int)); //Length of each of the arrays in Route
 
-
-
-
-
-	/*
-	for(int i = 0; i < P+1; i++){
-		if(arr[i] > global[0] && arr[i] < global[1]){
-
-		}else if(arr[i] > global[1] && arr[i] < global[2]){
-
-		}else if(arr[i] > global[2] && arr[i] < global[3]){
-
-		}else if(arr[i] > global[3] && arr[i] < global[4]){
-
-		}else if(arr[i] > global[4]){
-
-		}
+	for(int i = 0; i < size; i++){
+		if(arr[i] <= global[1]){
+			Route[0][Length[0]] = arr[i];
+			Length[0]++;
+		}else if(arr[i] <= global[2]){
+			Route[1][Length[1]] = arr[i];
+			Length[1]++;
+		}else if(arr[i] <= global[3]){
+			Route[2][Length[2]] = arr[i];
+			Length[2]++;
+		}else {
+			Route[3][Length[3]] = arr[i];
+			Length[3]++;
+		} 
 	}
-	*/
+
+	//Variables used for the exchange of data
+	int RouteIn[P][size];
+	int *LengthIn = calloc(P,sizeof(int));
+	//Registers
+	bsp_push_reg(RouteIn,sizeof(int)*P*size);
+	bsp_push_reg(LengthIn,sizeof(int)*P);
+	bsp_sync();
+	//Routing: sending the local data to the corresponding processor
+	for(int i = 0; i < P; i++){
+		bsp_put(i,&Length[i],LengthIn,p*sizeof(int),sizeof(int));
+		bsp_put(i,&Route[i],RouteIn,p*size*sizeof(int),sizeof(int)*size);
+	}
+	bsp_sync();
+	//Final merge of the arrays received in Routing 
+	int *m1 = merge(&RouteIn[0][0],&RouteIn[1][0],LengthIn[0],LengthIn[1]);
+	int *m2 = merge(&RouteIn[2][0],&RouteIn[3][0],LengthIn[2],LengthIn[3]);
+	int *m3 = merge(m1,m2,LengthIn[0]+LengthIn[1],LengthIn[2]+LengthIn[3]);
+	//DONE
+	toString(p,m3,LengthIn[0]+LengthIn[1]+LengthIn[2]+LengthIn[3]);
+
 	bsp_end();
 }
 	
 int main( int argc, char ** argv ) {
-	printf( "How many threads do you want started? There are %d cores available. (Max 4 cores)\n", bsp_nprocs() );
-    fflush( stdout );
-    scanf( "%d", &P );
-    if( P == 0 || P > bsp_nprocs() ) {
-        fprintf( stderr, "Cannot start %d threads.\n", P );
-        return EXIT_FAILURE;
-    }
+    P = 4;
 	bsp_init( &sort, argc, argv );
 	sort();
     return EXIT_SUCCESS;
